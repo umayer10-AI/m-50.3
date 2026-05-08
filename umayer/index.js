1f096beaa97a39e8f2ab71128bc9c84da1ab3434
umayer10-AI <mdumayer10@gmail.com>
2026-05-08 07:50:32 +0600

    add

--- umayer/index.js
+++ umayer/index.js
@@ -7,6 +7,16 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+const uri = "mongodb+srv://server4:USoNTGhCCL1STwSE@cluster0.ywso1qs.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
