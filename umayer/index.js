1f81c8c1f7a5f747cb387e075a1610cfe5f7ed2d
umayer10-AI <mdumayer10@gmail.com>
2026-05-08 07:56:57 +0600

    add

--- umayer/index.js
+++ umayer/index.js
@@ -17,6 +17,28 @@ const client = new MongoClient(uri, {
     }
 });
 
+const run = async () => {
+    try {
+        await client.connect();
+
+        const db = client.db("simpleClud4")
+        const userCollection = db.collection("users")
+
+        app.get("/user", async (req,res) => {
+            const cursor = await userCollection.find()
+            res.send(cursor)
+        })
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } 
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
