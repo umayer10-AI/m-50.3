73d7b733e777d9ca648cbc1c41685d9f6505ffc0
umayer10-AI <mdumayer10@gmail.com>
2026-05-08 08:22:18 +0600

    add

--- app/src/app/user/[id]/page.jsx
+++ app/src/app/user/[id]/page.jsx
@@ -1,13 +1,16 @@
+import { getId } from '@/lib/data';
 import React from 'react';
 
 const page = async ({params}) => {
 
     const {id} = await params
-    // console.log(id)
+    const data = await getId(id)
 
     return (
-        <div>
-            
+        <div className='p-4 rounded-2xl border w-fit border-yellow-300'>
+            <h2>Name: {data.name}</h2>
+            <h2>Email: {data.email}</h2>
+            <h2>Role: {data.role}</h2>
         </div>
     );
 };
