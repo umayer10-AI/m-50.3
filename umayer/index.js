be3ee77adebbb297745f16a8747d6c65459f3853
umayer10-AI <mdumayer10@gmail.com>
2026-05-08 07:49:41 +0600

    add

--- umayer/index.js
+++ umayer/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
